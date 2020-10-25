aca941e24e0c6859e3edc5899e23ae7c33f3b285
BuildTools <pepnieto97@gmail.com>
2020-10-25 22:18:42 +0100

    first commit, with first excersice done

--- react-2/2-10-roll-the-dice/src/App.js
+++ react-2/2-10-roll-the-dice/src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 // This returns one number randomly from 1 to 6
 
 function App() {
-  const [dices, setDices] = React.useState([1,1]);
-  const [numDices, setNumDices] = React.useState([2]);
   const tirarDado = () => Math.floor(Math.random() * 6) + 1;
+  const [dices, setDices] = React.useState([tirarDado(),tirarDado()]);
+  const [numDices, setNumDices] = React.useState([2]);
+ 
   const handleClick = () => {
     setDices(tirarDados()); 
   };
@@ -20,16 +21,16 @@ function App() {
 
         return arrayDices;
     }
-  const winCondition(array){
-    array.find() != 6;
-
-
-  }
+    const winCondition = array =>{
+      let check;
+      check = array.find(x => x !== 6 ) === undefined;
+      return check;
+    }
     
   
   return (
-    <div className="App">
-      <input value={numDices} type="number" onChange={e => setNumDices(e.currentTarget.value)}/>
+    <div className="App" > 
+      <input  value={numDices} type="number" onChange={e => setNumDices(e.currentTarget.value)}/>
       <button onClick={handleClick}>Tirar Dados</button>
       {dices.map((data, i) => <p>Alea iacta est {data} </p>)}
       {winCondition(dices) && <p>You win!</p>}
